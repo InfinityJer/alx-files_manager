9de9ec844edd776ee627a74a3e1003bfdcd1bf75
InfintyJer <89025169+InfinityJer@users.noreply.github.com>
2024-03-12 15:11:16 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
+
 
 const router = express.Router();
 
