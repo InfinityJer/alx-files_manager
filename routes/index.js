e3b561459b7af8ce9b143501c1c36b3edfc79c08
InfintyJer <89025169+InfinityJer@users.noreply.github.com>
2024-03-14 12:45:09 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -16,4 +16,12 @@ router.get('/users/me', UsersController.getMe);
 router.post('/users', UsersController.postNew);
 router.post('/files', FilesController.postUpload);
 
+router.get('/files/:id', FilesController.getShow); // GET /files/:id
+router.get('/files', FilesController.getIndex); // GET /files
+
+router.put('/files/:id/publish', FilesController.putPublish); // PUT /files/:id/publish
+router.put('/files/:id/unpublish', FilesController.putUnpublish); // PUT /files/:id/unpublish
+
+router.get('/files/:id/data', FilesController.getFile); // GET /files/:id/data
+
 export default router;
